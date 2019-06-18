106337ef36fd6d9044ce08d541d48caf17fb0b4a
wr20171105408 <2394889963@qq.com>
2019-06-18 11:59:28 +0800

    实现了动态图

--- Game/src/StartGame/Startgame.java
+++ Game/src/StartGame/Startgame.java
@@ -1,6 +1,11 @@
 package StartGame;
 
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import Main.mainPage;
 
@@ -8,10 +13,16 @@ public class Startgame extends mainPage{
 
 	public static void start(Object object) {
 		// TODO Auto-generated method stub
-		JFrame framehelp = new JFrame();
-		framehelp.setTitle("开始游戏");
-		framehelp.setLocation(300, 50);
-		framehelp.setSize(1280, 760);
-		framehelp.setVisible(true);
+		JFrame framestart = new JFrame();
+		framestart.setTitle("开始游戏");
+		framestart.setLocation(300, 50);
+		framestart.setSize(1280, 760);
+		framestart.setVisible(true);
+		ImageIcon icon = new ImageIcon("Images/mario_left.gif");//将图片放入label中
+		JLabel label = new JLabel(icon);//设置label的大小
+		label.setBounds(0,0,icon.getIconWidth(),icon.getIconHeight());
+		framestart.getLayeredPane().add(label,new Integer(Integer.MIN_VALUE));//获取窗口的第二层，将label放入	
+		JPanel j=(JPanel)framestart.getContentPane();//获取frame的顶层容器,并设置为透明
+		j.setOpaque(false);
 	}
 }
